85e66038d95cf715133a8fb0799bbdd1ab796a07
Ibrahim <ibrahimalkhalilridene.prepa@gmail.com>
2024-09-11 15:20:08 +0200

    l

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ MODEL_KWARGS = {"input_size": (256, 2206)} #size of a spectrogram/mel-spectrogra
 
 APPROACH = 2  # change the approach based on 2, 10 or 50 classes
 
-esc_dataset = ESCDataset(download=False) if APPROACH!=2 else ESCDatasetBin(download=False,data_size=100)
+esc_dataset = ESCDataset(download=False, data_size=100) if APPROACH!=2 else ESCDatasetBin(download=False,data_size=100)
 
 bardou_models = generate_models({"spectrogram": Spectrogram}, CNNBardou, "cnn_bardou")
 #zhang_models = generate_models(FEATURES, ConvolutionalRNNZhang, "crnn_zhang", classifier_kwargs=MODEL_KWARGS)
