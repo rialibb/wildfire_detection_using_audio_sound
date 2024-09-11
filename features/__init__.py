4763b8fa50bef3ceb2b93063eec834b773497de3
Gabrielle Caillaud <gabrielle.caillaud@student-cs.fr>
2024-09-11 10:33:14 +0200

    [DEV] add AST feature extraction

--- features/__init__.py
+++ features/__init__.py
@@ -3,3 +3,4 @@ from features.spectrogram import Spectrogram
 from features.mel_spectrogram import MelSpectrogram
 from features.mfcc import MFCC
 from features.PCA import PCA
+from features.ast_encoder import  AstEncoder
