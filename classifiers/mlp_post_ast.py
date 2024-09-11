9ab4949e5ea5a4b8d58e2d8d9727d83c31956ffa
adil.el-yaalaoui@student-cs.fr <elyaalaoui_adi@centralesupelec.fr>
2024-09-11 10:27:44 +0200

    add MLP for ast

---
+++ classifiers/mlp_post_ast.py
@@ -0,0 +1,32 @@
+import torch
+
+from classifiers.nn_utils import Flattening, LocalResponseNorm, CNNLayer
+
+
+class MLPPostAst(torch.nn.Module):
+    def __init__(self, input_size:tuple[int,int]) -> tuple[int,int]:
+
+
+        super(MLPPostAst,self).__init__()
+
+        self.flatten=Flattening()
+
+
+        self.linear_layers=torch.nn.Sequential(
+            torch.nn.Linear(in_features=input_size[0]*input_size[1],out_features=512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=512,out_features=256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=256,out_features=64),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=64,out_features=2),
+            torch.nn.Sigmoid()
+        )
+
+
+    def forward(self,x:torch.Tensor) -> torch.Tensor:
+
+        x=self.flatten(x)
+        x=self.linear_layers(x)
+
+        return x
