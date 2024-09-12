5608f9f9e13e2b49e71dbb94531e9f727c7c9fa0
Gabrielle Caillaud <gabrielle.caillaud@student-cs.fr>
2024-09-12 15:37:46 +0200

    [DEV] some comments

--- features/spectrogram.py
+++ features/spectrogram.py
@@ -1,35 +1,32 @@
 from typing import Optional
-
 import torch
 import torchaudio.transforms as transforms
 
-from helpers import duration_to_frame_numbers
-
 
 class Spectrogram(torch.nn.Module):
+
+    # the shape after the forward function.
     output_shape = (256, 2206)
+
     def __init__(
         self,
         num_frequencies: int = 256,
         window_size: int = 200,
-        hop_size: Optional[int]= None,
+        hop_size: Optional[int] = None,
     ):
         """Spectrogram feature-extractor
 
         Parameters
         ----------
         num_frequencies: int, optional
-            the number of frequencies to sample on. Defaults to 201.
-        window_length: int, optional
-            the length of the spectrogram window, in frames. Defaults to 10.
+            the number of frequencies to sample on. Defaults to 256.
+        window_size: int, optional
+            the length of the spectrogram window, in frames. Defaults to 200.
         hop_size: int, optional
             the distance between two spectrogram windows, in frames. Defaults to win_length/2.
         """
         super(Spectrogram, self).__init__()
         n_fft = 2 * (num_frequencies - 1)
-
-
-
         self.spec = transforms.Spectrogram(
             n_fft=n_fft, win_length=window_size, hop_length=hop_size, power=2
         )
